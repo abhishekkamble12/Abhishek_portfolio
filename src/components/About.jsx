f3407e0f81552e85e0e50749ca5cc1e5fb766cc4
abhishekkamble12 <145703447+abhishekkamble12@users.noreply.github.com>
2025-08-19 19:59:19 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => (
       Hello, I'm <span className="font-bold text-white">Abhishek Kamble</span> passionate about building smart and scalable web & mobile applications. I've completed a full-stack development course and constantly explore new technologies to refine my skills. Focused on continuous learning, I aim to transition into the IT industry by 2027 and eventually move towards AI and data science.
     </p>
     <div className="flex gap-6">
-      <a href="https://drive.google.com/file/d/1qt2Y8SzFHXbV31HVrNEIYPTPIutyJUQd/view?usp=drivesdk" download className="px-6 py-3 bg-purple-500 text-white rounded-lg shadow hover:bg-purple-600 transition">Download CV</a>
+      <a href="https://drive.google.com/file/d/1qt2Y8SzFHXbV31HVrNEIYPTPIutyJUQd/view?usp=drivesdk">Download CV</a>
       <a href="#portfolio" className="px-6 py-3 border border-purple-400 text-purple-400 rounded-lg hover:bg-purple-400 hover:text-white transition">View Projects</a>
     </div>
   </section>
